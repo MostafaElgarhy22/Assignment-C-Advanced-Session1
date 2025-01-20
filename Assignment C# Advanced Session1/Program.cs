7dc318522f1829dd23c7e0d4b0b42eedf70fe725
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-20 02:20:47 +0200

    Q 1

--- Assignment C# Advanced Session1/Program.cs
+++ Assignment C# Advanced Session1/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Assignment_C__Advanced_Session1
+﻿using Assignment_C__Advanced_Session1.Q_1;
+
+namespace Assignment_C__Advanced_Session1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-          int[] Arr
+            #region Q 1
+            int[] Numbers = { 100, 56, 22, 17, 13, 90, 99 };
+
+            Helper<int>.BubbleSort(Numbers);
+
+
+            foreach (int number in Numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            #endregion
+
         }
     }
 }
