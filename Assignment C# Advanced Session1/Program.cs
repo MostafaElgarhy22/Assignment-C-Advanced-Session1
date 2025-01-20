900624d45923e74def458bfc72255de62efb2606
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-20 02:01:43 +0200

    Add project files.

---
+++ Assignment C# Advanced Session1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_C__Advanced_Session1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          int[] Arr
+        }
+    }
+}
---
+++ Assignment C# Advanced Session1/Q 1/BubbleSort.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_C__Advanced_Session1.Q_1
+{
+    public static class Helper<T> where T : IComparable<T>
+    {
+        public static void Swap(ref T a, ref T b)
+        {
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+    public static class BubbleSortt
+    {
+        public static void BubbleSort<T>(T[] arr) where T : IComparable<T>
+        {
+            if (arr is null || arr.Length == 0)
+                return;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool swapped = false;
+
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                {
+                    if (arr[j].CompareTo(arr[j + 1]) > 0)
+                    {
+                        Helper<T>.Swap(ref arr[j], ref arr[j + 1]);
+                        swapped = true;
+                    }
+                }
+            }
+
+        }
+    }
+}
